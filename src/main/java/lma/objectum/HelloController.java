5223eedc18735f325e129a1e6050918d9c0fba55
TrieuCvn <cvntrieu@gmail.com>
2024-10-11 20:40:52 +0700

    Commit to main

---
+++ src/main/java/lma/objectum/HelloController.java
@@ -0,0 +1,14 @@
+package lma.objectum;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module lma.objectum {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens lma.objectum to javafx.fxml;
+    exports lma.objectum;
+}
