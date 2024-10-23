1d122d0bb816ed0942ef7b06650192f21c415dc2
TrieuCvn <cvntrieu@gmail.com>
2024-10-23 22:06:12 +0700

    Saving changes in HelloApplication.java

--- src/main/java/lma/objectum/HelloApplication.java
+++ src/main/java/lma/objectum/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fxml/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 850, 650);
-        stage.setTitle("Hello!");
+        stage.setTitle("Objectum Library");
         stage.setScene(scene);
         stage.show();
     }
